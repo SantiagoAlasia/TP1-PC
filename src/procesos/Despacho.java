1382d29e52a63739e5efb725d750cd7640b4d33f
SantiagoAlasia <santiago.alasia@mi.unc.edu.ar>
2025-04-24 19:08:43 -0300

    Update Despacho.java
    Implementacion de la probabilidad

--- src/procesos/Despacho.java
+++ src/procesos/Despacho.java
@@ -1,19 +1,28 @@
+import java.util.Random;
+
 public class Despacho() extends  Proceso{
     private int probError;
     private MatrizCasilleros matrizCasilleros;
+    private random;
+    
 
     public Despacho(RegistroPedidos registros, int demora, MatrizCasilleros matrizCasilleros,int cantidadPedidos, int probError) {
         super(registros, demora, cantidadPedidos);
         this.matrizCasilleros = matrizCasilleros;
         this.probError = probError;
+        this.random = new Random();
     }
 
     @Override
     public void run() {
         while (registros.getCantidadPedidos(5) < cantidadPedidosMax && registros.getCantidadPedidos(0) != 0) {
-            // En base a la probabilidad de error, cambiar de colas un registro
-            // Cambia el estado del casillero a vacio
-            // Si entra en los casos de error, cambiar a fuera de servicio
+            if(random.nextDouble() > probError)){ // Actua en base a la probabilidad de error
+                // Cambiar un pedido del registro de preparacion al registro en transito
+                // Pone vacio el casillero
+            } 
+            else{
+                // Si entra en los casos de error, cambiar a fuera de servicio    
+            }
             demorar(); // Manda al hilo a dormir
         }
     }
